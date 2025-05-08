ec8aa89dc80467b331d1760f9443188b344518f0
Tammam20 <whymewhyalwaysme@protonmail.com>
2025-05-08 15:59:09 +0300

    Update .flatpak/remove-fedora-repo.sh

--- dot_flatpak/remove-fedora-repo.sh
+++ dot_flatpak/remove-fedora-repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 flatpak install --reinstall flathub $(flatpak list --app-runtime=org.fedoraproject.Platform --columns=application | tail -n +1 )
-flatpak remote-delete fedora
+flatpak remote-delete fedora fedora-testing
 ### restart gnome-software
 pkill gnome-software && gnome-software
 ### if it list an app to remove
