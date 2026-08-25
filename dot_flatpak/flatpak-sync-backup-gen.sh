bab6b34be735997eb4293263281e3128925b3520
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-08-25 14:25:09 +0300

    Remove .flatpak/flatpak-sync-backup-gen.sh Remove .flatpak/flatpak-sync-backup.sh Remove .flatpak/remove-fedora-repo.sh Remove .flatpak/theme.sh

--- dot_flatpak/flatpak-sync-backup-gen.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-flatpak list --all --columns=origin --columns=application | awk '{print "flatpak install " $1,$2 " -y"}' > ~/.flatpak/flatpak-sync-backup.sh
--- dot_flatpak/theme.sh
+++
@@ -1,2 +0,0 @@
-flatpak install flathub -y org.gtk.Gtk3theme.Adwaita-dark
-gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
